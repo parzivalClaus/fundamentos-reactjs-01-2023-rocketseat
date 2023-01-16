f20eacf470ec74f425b67d9a7efb4ce863051fec
parzivalClaus <cldgncvs91@gmail.com>
2023-01-16 10:15:49 -0300

    feat(Sidebar)

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -6,12 +7,15 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/parzivalClaus.png" />
+
                 <strong>Claudio Ferreira</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
